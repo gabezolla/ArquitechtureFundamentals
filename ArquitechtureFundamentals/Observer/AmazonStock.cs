de898859558c3d141ae3b6074a75bc61422a9c27
gabezolla <gaabe.zolla@hotmail.com>
2024-08-07 01:56:07 -0300

    add comment on concrete subject

--- ArquitechtureFundamentals/Observer/AmazonStock.cs
+++ ArquitechtureFundamentals/Observer/AmazonStock.cs
@@ -1,5 +1,8 @@
 ﻿namespace ArchitectureStudies.Observer
 {
+    /// <summary>
+    /// Concrete subject
+    /// </summary>
     public class AmazonStock(double price) : Stock("AMZN", price)
     {
     }
